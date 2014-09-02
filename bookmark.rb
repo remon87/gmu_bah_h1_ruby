9776d00323e47d3009c689d7df57cc2459b9e33a
remon87 <remon87@users.noreply.github.com>
2014-09-01 22:11:49 -0400

    Create bookmark.rb
    File without code.

---
+++ bookmark.rb
@@ -0,0 +1,9 @@
+# Bookmark to do the following:
+
+# An initializer that accepts url and title parameters:
+# 1) It should set instance variables named url, title
+# 2) It should set a read only variable name last_visited
+
+# A method name visit! which sets last_visited to the current time and date
+# (hint: the Time.now method returns an instance of Time)
+
