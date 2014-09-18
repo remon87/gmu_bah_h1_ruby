411ec71e438d0caf1ea5d2b3085345be6b96679f
remon87 <remon87@users.noreply.github.com>
2014-09-18 14:25:07 -0400

    Update bookmark.rb

--- bookmark.rb
+++ bookmark.rb
@@ -7,3 +7,20 @@
 # A method name visit! which sets last_visited to the current time and date
 # (hint: the Time.now method returns an instance of Time)
 
+Class Bookmark
+attr_reader :last_visited
+
+  def initializer(url, title)
+    @url = url
+    @title = title
+    last_visited.method(:visit).call 
+  end
+  
+  def visit(last_visited)
+    last_visited = Time.now
+  end
+  
+  
+  
+  
+end
